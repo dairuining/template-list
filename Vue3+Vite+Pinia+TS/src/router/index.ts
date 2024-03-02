e2755574955c438a867fa250a84993b6a50bc2aa
dairuining <2436480281@qq.com>
2024-03-02 16:00:55 +0800

    feat: 在Vue3+Vite+Pinia+TS模板中执行pnpm eslint命令

--- Vue3+Vite+Pinia+TS/src/router/index.ts
+++ Vue3+Vite+Pinia+TS/src/router/index.ts
@@ -1,13 +1,15 @@
 import { createRouter, createWebHashHistory, RouteRecordRaw } from 'vue-router';
 
 // 配置路由
-const routes: Array<RouteRecordRaw> = [{
-  path: '/',
-  name: 'Home',
-  component: () => import('@/views/home/index.vue'),
-  meta: {},
-  children: [],
-}];
+const routes: Array<RouteRecordRaw> = [
+  {
+    path: '/',
+    name: 'Home',
+    component: () => import('@/views/home/index.vue'),
+    meta: {},
+    children: [],
+  },
+];
 
 const router = createRouter({
   history: createWebHashHistory(),
--- Vue3+Vite+Pinia+TS/src/store/user/index.ts
+++ Vue3+Vite+Pinia+TS/src/store/user/index.ts
@@ -2,25 +2,25 @@ import { defineStore } from 'pinia';
 import pinia from '@/store';
 
 export interface UserState {
-    username: string;
-    accessToken: string;
-    refreshToken?: string;
-    roles: Array<string>;
+  username: string;
+  accessToken: string;
+  refreshToken?: string;
+  roles: Array<string>;
 }
 export const useUserStoreHook = defineStore('userInfo', {
-    state: (): UserState => ({
-        username: 'dairuining',
-        accessToken: '',
-        roles: ['common']
-    }),
-    getters: {},
-    actions: {},
-    persist: {
-        key: 'userInfo',
-        storage: sessionStorage,
-        paths: ['accessToken']
-    }
+  state: (): UserState => ({
+    username: 'dairuining',
+    accessToken: '',
+    roles: ['common'],
+  }),
+  getters: {},
+  actions: {},
+  persist: {
+    key: 'userInfo',
+    storage: sessionStorage,
+    paths: ['accessToken'],
+  },
 });
 export function useUserStore() {
-    return useUserStoreHook(pinia);
+  return useUserStoreHook(pinia);
 }
