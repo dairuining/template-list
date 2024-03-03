4571fdd552eb14714a54972f69ab06fbdc1fc5e1
dairuining <2436480281@qq.com>
2024-03-03 17:32:49 +0800

    feat: 惬意styles文件夹到assets文件夹下

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,7 @@ import { createApp } from 'vue';
 import App from './App.vue';
 import pinia from './store';
 import router from './router';
-import './styles/index.less';
+import './assets/styles/index.less';
 
 const app = createApp(App);
 app.use(router);
