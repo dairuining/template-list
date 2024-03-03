a7f11bd3418003c0296700d0244139a275845348
dairuining <2436480281@qq.com>
2024-03-03 17:49:46 +0800

    新增readme文件

---
+++ README.md
@@ -0,0 +1,7 @@
+# 创建开发模板
+
+- Vue3-Vite-Pinia-TS 特性
+
+- Vue3-Webpack5-TS 特性
+
+- React-Webpack5-TS 特性
--- src/components/auto/a-button.vue
+++
@@ -1,7 +0,0 @@
-<template>
-  <div>{{ count }}</div>
-</template>
-
-<script lang="ts" setup>
-const count = ref(0);
-</script>
